61137344c153f2bde42484ed390ddd510cf3821e
laibaatiq017 <156577288+laibaatiq017@users.noreply.github.com>
2024-01-14 15:07:09 +0500

    Add files via upload

---
+++ bano qabil.py
@@ -0,0 +1,24 @@
+("""addition,
+ subatraction
+ multiplication
+ divison""")
+
+num1=int(input("enter the value of num1"))
+num2=int(input("enter the value of num2"))
+opr=input("enter the opr ...")
+
+if opr == "+":
+   print(num1+num2)
+elif opr == "-":
+     print(num1-num2)
+elif opr == "*":
+     print(num1*num2)
+elif opr == "/":
+     print(num1/num2)
+else:
+    print("invalid error")
+   
+
+
+
+    
